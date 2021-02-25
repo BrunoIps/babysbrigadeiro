2df9eff48964c1a85875f613d08422a3b5f485f6
BrunoIps <brunokoyanip@gmail.com>
2021-02-24 22:49:03 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+var brig = $('.brigadeiro-img');
+setInterval(() => {
+  
+  brig.attr('src', 'src/images/babybrigadeiro.png').hide();
+  setTimeout(()=>{
+    brig.show('slide')
+  }, 4000)
+
+}, 3500);
+  
+//   rotate = 0;
+// brig.mouseenter( () =>{
+//   for(var i = 0; i < 24; i++){
+//     rotate += 15;
+//     brig.css({'TransitionDuration':'1s'})
+//     brig.css({'transform': 'rotate(' + rotate+'deg)'})
+//   }
+//})
+
+$('.btn-whats').click(()=>{
+  console.log("click")
+  window.open('https://api.whatsapp.com/send?phone=5519996020388&text=Ol%C3%A1%20gostaria%20de%20informa%C3%A7%C3%B5es%20dos%20doces')
+})
